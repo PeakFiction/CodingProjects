c5fe694abda29d52cf2ef1e9c3afeeff42fe8d80
Muhammad Sakhran <sakhran.t@gmail.com>
2023-10-30 11:43:33 +0700

    1352C in Progress

--- Python/Codeforces Problems/test.py
+++ Python/Codeforces Problems/test.py
@@ -1,19 +1,6 @@
-import math
-n = 101
-m = 110
-b = 100
+my_list = [1, 2, 3, 4, 5]
 
+# Pick the last element using negative indexing
+last_element = my_list[-1]
 
-Special = (n // m) * b 
-RemainderQuant = (n % m)
-
-if RemainderQuant < m or RemainderQuant == m:
-    Remainder = b * 1
-else:
-    Remainder = math.ceil(n/m) * b
-SpecialOnly = Special + Remainder
-print(n%m)
-print("Special: ", Special)
-print("Remainder: ", Remainder)
-
-print(SpecialOnly)
+print(last_element)
